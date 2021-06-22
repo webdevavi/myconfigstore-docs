33c16d0e542d290e483ef7245f74f2b647da9675
devavi-labs <devavilabs@gmail.com>
2021-06-22 20:21:36 +0530

    refactor: theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,31 @@
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import Layout from "@theme/Layout";
-import clsx from "clsx";
 import React from "react";
 import styles from "./index.module.css";
 
-function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
-  return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Get Started - 5min ⏱️
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
-
 export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+
   return (
     <Layout
       title="Docs"
       description="Description will go into a meta tag in <head />"
     >
-      <HomepageHeader />
+      <div className={styles.heroBanner}>
+        <h1 className="hero__title">
+          <span className="hero__title-dark">my</span>config
+        </h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link
+            className="button button--primary button--lg button--hero"
+            to="/docs/intro"
+          >
+            Get Started
+          </Link>
+        </div>
+      </div>
     </Layout>
   );
 }
